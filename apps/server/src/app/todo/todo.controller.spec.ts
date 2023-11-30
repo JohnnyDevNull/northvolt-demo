24187945eaca93cdb0be8dde3503f93f3ed70660
Philipp John <philipp.john@live.de>
2023-11-30 22:22:16 +0100

    fix(client+server): fix unit tests

--- apps/server/src/app/todo/todo.controller.spec.ts
+++ apps/server/src/app/todo/todo.controller.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Todo } from './entities/todo.entity';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
 
@@ -8,7 +10,16 @@ describe('TodoController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodoController],
-      providers: [TodoService],
+      providers: [
+        TodoService,
+        {
+          provide: getRepositoryToken(Todo),
+          useValue: {
+            create: jest.fn(v => v),
+            save: jest.fn(v => v),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<TodoController>(TodoController);
--- apps/server/src/app/todo/todo.service.spec.ts
+++ apps/server/src/app/todo/todo.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Todo } from './entities/todo.entity';
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
@@ -6,7 +8,16 @@ describe('TodoService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoService],
+      providers: [
+        TodoService,
+        {
+          provide: getRepositoryToken(Todo),
+          useValue: {
+            create: jest.fn(v => v),
+            save: jest.fn(v => v),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<TodoService>(TodoService);
