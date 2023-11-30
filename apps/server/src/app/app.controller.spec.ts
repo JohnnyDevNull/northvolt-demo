889849a5f25928f2c3e03f8ada1d398a4e596bbd
Philipp John <philipp.john@live.de>
2023-11-30 20:10:55 +0100

    refactor: rename root route message

--- apps/server/src/app/app.controller.spec.ts
+++ apps/server/src/app/app.controller.spec.ts
@@ -14,9 +14,9 @@ describe('AppController', () => {
   });
 
   describe('getData', () => {
-    it('should return "Hello API"', () => {
+    it('should return "The API is online!"', () => {
       const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({ message: 'Hello API' });
+      expect(appController.getData()).toEqual({ message: 'The API is online!' });
     });
   });
 });
--- apps/server/src/app/app.service.spec.ts
+++ apps/server/src/app/app.service.spec.ts
@@ -14,8 +14,8 @@ describe('AppService', () => {
   });
 
   describe('getData', () => {
-    it('should return "Hello API"', () => {
-      expect(service.getData()).toEqual({ message: 'Hello API' });
+    it('should return "The API is online!"', () => {
+      expect(service.getData()).toEqual({ message: 'The API is online!' });
     });
   });
 });
