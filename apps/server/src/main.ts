8708bc10eedf4fb6451b978cafc56ca2dd2977e3
Philipp John <philipp.john@live.de>
2023-11-30 20:07:31 +0100

    build: add dot env for server

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,19 +1,22 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from '@nestjs/core';
-
+import helmet from 'helmet';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const config = app.get(ConfigService);
+
+  const globalPrefix = config.get<string>('API_PREFIX');
+  const port = config.get<number>('API_PORT');
+
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
