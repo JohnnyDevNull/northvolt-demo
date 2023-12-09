9bcb0f4304a2dfd23fdc43425ec7201667af0124
JohnnyDevNull <philipp.john@live.de>
2023-12-09 09:48:04 +0100

    feat(client-react): implement server hello component

--- apps/client-react/src/app/pages/server-hello/server-hello.tsx
+++ apps/client-react/src/app/pages/server-hello/server-hello.tsx
@@ -1,7 +1,29 @@
+import { IServerResponse } from '@northvolt/shared';
+import { useEffect, useState } from 'react';
+
+const initialServerMessageState: IServerResponse | null = null;
+const initialErrorState: string | null = null;
+
 export default function ServerHello() {
+  const [serverMessage, setServerMessage] = useState(initialServerMessageState);
+  const [error, setError] = useState(initialErrorState);
+
+  /**
+   * Note: the empty deps array [] means
+   * this useEffect will run once
+   * similar to componentDidMount()
+   */
+  useEffect(() => {
+    fetch('/api')
+      .then(res => res.json())
+      .then((result: IServerResponse) => setServerMessage(result))
+      .catch((error: Error) => setError('Error while requesting API'));
+  }, []);
+
   return (
     <div>
-      Server Hello
+      {error && <span>{error}</span>}
+      {serverMessage && <pre>{JSON.stringify(serverMessage, null, 2)}</pre>}
     </div>
   );
 }
